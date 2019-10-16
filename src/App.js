9d6f93808e61522ead5c9fe8738dfae68749c1fe
digi0ps <sriru1998@gmail.com>
2019-10-16 11:59:49 +0530

    Fetch and display images.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const BASE_URL = "http://127.0.0.1:8000";
+
+  useEffect(() => {
+    async function fetchData() {
+      const url = `${BASE_URL}/api/gallery/`;
+      const response = await axios(url);
+
+      setPhotos(response.data);
+      setLoading(false);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">FindUs - People Gallery</header>
+      <main>
+        {loading ? "Your photos are loading" : null}
+        {photos.map(photo => (
+          <div className="photo" key={photo.id}>
+            <img
+              alt="Gallery"
+              height="300px"
+              src={`${BASE_URL}${photo.image}`}
+            />
+            <br />
+            People in picture: {photo.persons.person_name}
+            <hr />
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
